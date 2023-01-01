dbcdfb67d03239691cd121d3a9a496d485416b25
Tarsis <br.tmvdl@gmail.com>
2023-01-01 17:53:11 -0300

    Fixing PULL script

--- env/pull.sh
+++ env/pull.sh
@@ -1,5 +1,9 @@
 mkdir -p src
 
-gh repo clone brtmvdl/plus-www ./src/www
+git clone git@github.com:brtmvdl/plus-www.git ./src/www
 
-gh repo clone brtmvdl/plus-app ./src/app
+# gh repo clone brtmvdl/plus-www ./src/www
+
+git clone git@github.com:brtmvdl/plus-app.git ./src/app
+
+# gh repo clone brtmvdl/plus-app ./src/app
