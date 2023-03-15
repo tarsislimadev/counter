6bbd488372263c4a5e1e9d7ef8a599c84943392a
Tarsis <br.tmvdl@gmail.com>
2023-03-15 13:49:28 -0300

    Fixing scripts

--- env/pull.sh
+++ env/pull.sh
@@ -1,5 +1,3 @@
 mkdir -p src
 
-git clone git@github.com:brtmvdl/plus-www.git ./src/www
-
-# git clone git@github.com:brtmvdl/plus-app.git ./src/app
+git clone git@github.com:brtmvdl/counter-www.git ./src/www
